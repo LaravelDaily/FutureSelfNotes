cec55d0f8a7af835a6ea4f8623bb702dc2621fc2
Povilas Korop <povilask007@gmail.com>
2020-03-04 20:15:36 +0200

    Mailgun attempt

--- config/mail.php
+++ config/mail.php
@@ -60,6 +60,12 @@ return [
         'array' => [
             'transport' => 'array',
         ],
+
+        'mailgun' => [
+            'domain' => env('MAILGUN_DOMAIN'),
+            'secret' => env('MAILGUN_SECRET'),
+            'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        ],
     ],
 
     /*
