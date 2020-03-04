61c8bd96d1abd21d2b9e231e2f803fa37355772c
Povilas Korop <povilask007@gmail.com>
2020-03-04 15:36:52 +0200

    Note tag migration

---
+++ database/migrations/2020_03_04_133542_create_note_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoteTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('note_tag', function (Blueprint $table) {
+            $table->unsignedBigInteger('note_id');
+            $table->foreign('note_id')->references('id')->on('notes');
+            $table->unsignedBigInteger('tag_id');
+            $table->foreign('tag_id')->references('id')->on('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('note_tag');
+    }
+}
